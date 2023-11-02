2747691fe480ff7c868e9c3b5f84c6240ce9c119
Trapkhing <148591599+Trapkhing@users.noreply.github.com>
2023-11-02 14:36:01 +0000

    Update app.py

--- app.py
+++ app.py
@@ -8,4 +8,5 @@ def trap():
 def about():
     return app.send_static_file ("about.html")
 if __name__ == '__main__':
-    app.run(host="127.0.0.1",port="8080")
+    from waitress import serve
+    serve(app,host='0.0.0.0',port=8080)
